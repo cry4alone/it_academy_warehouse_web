241a9c3754a292502954033a0d08aca041a1e5bb
cry4alone <chernenkoa04@gmail.com>
2024-11-18 18:47:50 +0700

    мини фиксы

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom"; // Используем BrowserRouter
 import Header from "./header/Header";
-import Footer from "./Footer";
 import Auth from "../pages/Auth";
 import NotFoundPage from "../pages/NotFoundPage";
+import "../styles/global.scss";
 import Sidebar from "./sidebar/Sidebar";
 
 class App extends Component {
@@ -11,13 +11,12 @@ class App extends Component {
         return (
             <BrowserRouter>
                 <Header />
-                {<Sidebar />}
+                <Sidebar />
                 <Routes>
                     <Route index element={<Auth />} /> 
                     <Route path="/auth" element={<Auth />} />
                     <Route path="*" element={<NotFoundPage />} /> 
                 </Routes>
-                <Footer />
             </BrowserRouter>
         );
     }
