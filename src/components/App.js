0f597fbabad10d18792e1ce0d31d426342e8689a
Koval_Dan <koval_dan2004@mail.ru>
2024-11-18 15:18:44 +0700

    реализация компонента Sidebar

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,20 @@ import Header from "./header/Header";
 import Footer from "./Footer";
 import Auth from "../pages/Auth";
 import NotFoundPage from "../pages/NotFoundPage";
-import "../styles/global.scss";
+import Sidebar from "./sidebar/Sidebar";
 
 class App extends Component {
     render() {
         return (
             <BrowserRouter>
                 <Header />
+                {<Sidebar />}
                 <Routes>
                     <Route index element={<Auth />} /> 
                     <Route path="/auth" element={<Auth />} />
                     <Route path="*" element={<NotFoundPage />} /> 
                 </Routes>
+                <Footer />
             </BrowserRouter>
         );
     }
