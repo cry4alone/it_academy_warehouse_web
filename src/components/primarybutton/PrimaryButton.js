7dd56ba39c0bce57ecaed6537df726ca6ca65246
cry4alone <chernenkoa04@gmail.com>
2024-11-19 21:37:01 +0700

    Убраны ненужные зависимости

--- src/components/primarybutton/PrimaryButton.js
+++ src/components/primarybutton/PrimaryButton.js
@@ -1,19 +1,12 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './PrimaryButton.scss';
+import React from "react";
+import "./PrimaryButton.scss";
 
-const PrimaryButton = ({ onClick, children, type = 'button' }) => {
+const PrimaryButton = ({ onClick, children, type = "button" }) => {
   return (
     <button className="primarybutton" onClick={onClick} type={type}>
       {children}
     </button>
   );
-};
-
-PrimaryButton.propTypes = {
-  onClick: PropTypes.func.isRequired,
-  children: PropTypes.node.isRequired,
-  type: PropTypes.string,
 };
 
 export default PrimaryButton;
